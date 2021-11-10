fd3dba0473bdf6a8ca3ea713eefeab59ffb69c22
andre <andre.bertacchi@gmail.com>
2021-11-10 09:06:31 -0300

    primeiro commit

---
+++ calculadora_medias.py
@@ -0,0 +1,27 @@
+import math
+n=int(0)
+vetor=[]
+valor=float(input("Digite um valor diferente de zero: "))
+while(valor!=0):
+  vetor.append(valor)
+  n+=1
+  valor=float(input("Digite um valor diferente de zero: "))
+
+for i in range(len(vetor)):
+  print("valor "+str(i)+" "+str(vetor[i]))
+
+print("")
+media=float(0)
+for i in range(len(vetor)):
+  media=media+vetor[i]
+
+media=media/n
+
+desvio=float(0)
+for i in range(len(vetor)):
+  desvio=desvio+(media-vetor[i])**2
+
+desvio=desvio/(n*(n-1))
+desvio=math.sqrt(desvio)
+
+print("Média: "+str(media)+", desvio-padrão: "+str(desvio))
